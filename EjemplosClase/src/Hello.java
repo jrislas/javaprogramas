3c6ff6edb69dc2d48fc238f9d9b40bd65c985492
jrislas <jrislas@gmail.com>
2016-06-15 15:11:11 -0500

    cambio6

--- EjemplosClase/src/Hello.java
+++ EjemplosClase/src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 	
 	public static void main(String[] args) {
         System.out.println("Hello, World Richard y Luis y carmen +eliuth!+Chikis");
+        System.out.println("Hello, coomo estas"); 
+        
     }
 
 	
