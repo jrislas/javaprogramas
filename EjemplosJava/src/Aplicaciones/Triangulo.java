aab8f9b82255718bbadcd04e12dc49d2720b2d0a
jrislas <jrislas@gmail.com>
2016-06-20 11:01:38 -0500

    Envio

---
+++ EjemplosJava/src/Aplicaciones/Triangulo.java
@@ -0,0 +1,18 @@
+package Aplicaciones;
+import EntradaDatos.*;
+import java.io.*;
+
+public class Triangulo {
+
+	public static void main(String[] args) {
+	
+		int numero=0;
+		
+		System.out.print("Introduce un numero");
+		numero= Entrada.leerEntero();
+		System.out.print("El numero es"+numero);
+		
+		
+	}
+
+}
