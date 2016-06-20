77be20781d1b768a09393d894bcf4554fa6ef8ba
jrislas <jrislas@gmail.com>
2016-06-19 23:29:30 -0500

    cambios

---
+++ EjemplosClase/src/Entrada.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner;
+
+
+
+public class Entrada {
+
+	private static Scanner Lector = new Scanner(System.in);
+	public static int leerEntero(){
+		return Lector.nextInt();
+
+	}
+
+}
